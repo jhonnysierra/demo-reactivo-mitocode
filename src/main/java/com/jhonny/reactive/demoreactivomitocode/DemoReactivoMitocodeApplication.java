cdde2080c9f7370e30a014ffa6070bbea6627580
Jhonny Sierra <jhonnysierrap@gmail.com>
2023-06-14 14:42:28 -0500

    Update Run Application
    Se modifica el run application para que siga mostrando en consola los ejercicios del curso que imprimen datos en consola con el arranque de la aplicación

--- src/main/java/com/jhonny/reactive/demoreactivomitocode/DemoReactivoMitocodeApplication.java
+++ src/main/java/com/jhonny/reactive/demoreactivomitocode/DemoReactivoMitocodeApplication.java
@@ -1,19 +1,17 @@
 package com.jhonny.reactive.demoreactivomitocode;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.jhonny.reactive.demoreactivomitocode.pruebascurso.PruebasCurso;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pruebascurso.PruebasCurso;
 
 @SpringBootApplication
 public class DemoReactivoMitocodeApplication implements CommandLineRunner {
-	private static final Logger log = LoggerFactory.getLogger(DemoReactivoMitocodeApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoReactivoMitocodeApplication.class, args);
 	}
+
 
 
 	@Override
